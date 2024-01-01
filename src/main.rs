f1943679b331f04c91171fab4ae620f29409a991
Maxime Delporte <maxime@delporte.dev>
2024-01-01 22:49:13 +0100

    create root endpoint returning Hello World message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(hello_world));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn hello_world() -> &'static str {
+    "Hello, Friend!"
 }
